dbd9b39a756f9fd2c5c2a3d1a16d2c2cc26ea9c8
Darshan makvana <darshanmakvana9801@gmail.com>
2025-06-15 22:13:43 +0530

    Add files via upload

---
+++ 13-6-2025/main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>//header file
+#include<conio.h>//header file
+void main ()//main function
+{
+	//datatype variable = value;
+
+
+	int num1 = 30;
+	int num2 = 20;
+	int num3 = 10;
+	int num4 = 60;
+	
+        printf ("\ n your number is:%d ",num1);
+        printf ("\ n your number is:%d ",num2);
+        printf ("\ n your number is:%d ",num3);
+        printf ("\ n your number is:%d ",num4);
+        
+        getch();// terminate the programe 
+        
+        }
