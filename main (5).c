8e5b6e42e0acb7a9bdc0af49bcc4efe287fd8681
Darshan makvana <darshanmakvana9801@gmail.com>
2025-06-15 21:32:55 +0530

    Add files via upload

---
+++ main (5).c
@@ -0,0 +1,16 @@
+#include<stdio.h>//header file
+#include<conio.h>//header file 
+void main()//main function 
+{
+    printf("\n Details;");
+    
+    
+    printf("\n krish: \tflutter");
+    printf("\n darshan:\tflutter");
+    printf("\n mukhater:\tpython");
+    printf("\n danish \tpython");
+    
+    
+    
+    getch();//terminate the program 
+}
