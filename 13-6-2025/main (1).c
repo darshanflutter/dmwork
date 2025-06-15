d1413603d1157dce7b2c5bc3b49ad34e475abcc8
Darshan makvana <darshanmakvana9801@gmail.com>
2025-06-15 22:58:31 +0530

    Add files via upload

---
+++ 13-6-2025/main (1).c
@@ -0,0 +1,24 @@
+ #include<stdio.h>//headerfile
+ #include<conio.h>//headerfile
+ 
+ void main()//main function
+ {
+     int nub1 = 6;
+     int nub2 = 3;
+     int add,sub,mul,div1;
+     
+     add=nub1+nub2;
+     sub=nub1-nub2;
+     mul=nub1*nub2;
+     div1=nub1/nub2;
+     
+     printf("\n Addition is : %d", add);
+     printf("\n Substraction is : %d" ,sub);
+     printf("\n Multiplication is : %d" ,mul);
+     printf("\n Division is : %d" , div1);
+     
+     
+     getch();//terminate the program
+    
+    
+ }
