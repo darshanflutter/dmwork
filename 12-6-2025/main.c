563a920118c09f70cc36123c443c1b0c02c677f2
Darshan makvana <darshanmakvana9801@gmail.com>
2025-06-15 16:13:14 +0530

    Add files via upload

---
+++ 12-6-2025/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+    printf("\n your name : darshan ");
+    printf("\n your age: 24");
+    printf("\n your city: rajkot");
+    
+ getch(); 
+}
