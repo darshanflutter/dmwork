991d13c3876557ab0c8e556253526db825a71902
Darshan makvana <darshanmakvana9801@gmail.com>
2025-06-16 12:37:23 +0530

    Add files via upload

---
+++ 13-6-2025/main (2).c
@@ -0,0 +1,12 @@
+#include<stdio.h>//header file
+#include<conio.h>//header file 
+void main()//main function
+{
+    int maths=75,sci=85,guj=67,hin=87,eng=56,total,avg;
+    total = maths+sci+guj+hin+eng;
+    
+    printf ("\n total is : %d",total);
+    avg=total/5;
+    printf ("\n percentage is : %d",avg);
+    getch();// terminate the program 
+}
